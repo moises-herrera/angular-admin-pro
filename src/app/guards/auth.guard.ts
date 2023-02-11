0a97807c3d2ff28ef8594efbbeba2ae09fde8832
moises-herrera <moisesherrera037@gmail.com>
2023-02-11 12:47:26 -0500

    update routes

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,8 +2,11 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
+  Route,
   Router,
   RouterStateSnapshot,
+  UrlSegment,
   UrlTree,
 } from '@angular/router';
 import { Observable, tap } from 'rxjs';
@@ -12,8 +15,25 @@ import { UserService } from '../services/user.service';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private userService: UserService, private router: Router) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.userService.validateToken().pipe(
+      tap((isAuthenticated) => {
+        if (!isAuthenticated) {
+          this.router.navigateByUrl('/login');
+        }
+      })
+    );
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,81 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../guards/auth.guard';
 
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { ChartComponent } from './chart/chart.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UsersComponent } from './management/users/users.component';
-import { DoctorsComponent } from './management/doctors/doctors.component';
-import { HospitalsComponent } from './management/hospitals/hospitals.component';
 import { PagesComponent } from './pages.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ProgressComponent } from './progress/progress.component';
-import { PromisesComponent } from './promises/promises.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { DoctorComponent } from './management/doctors/doctor/doctor.component';
-import { SearchComponent } from './search/search.component';
-import { AdminGuard } from '../guards/admin.guard';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: PagesComponent,
     canActivate: [AuthGuard],
-    children: [
-      { path: '', component: DashboardComponent, data: { title: 'Dashboard' } },
-      {
-        path: 'progress',
-        component: ProgressComponent,
-        data: { title: 'Progress Bar' },
-      },
-      {
-        path: 'chart',
-        component: ChartComponent,
-        data: { title: 'Chart' },
-      },
-      {
-        path: 'account-settings',
-        component: AccountSettingsComponent,
-        data: { title: 'Account Settings' },
-      },
-      {
-        path: 'search/:term',
-        component: SearchComponent,
-        data: { title: 'Search' },
-      },
-      {
-        path: 'promises',
-        component: PromisesComponent,
-        data: { title: 'Promises' },
-      },
-      { path: 'rxjs', component: RxjsComponent, data: { title: 'RxJs' } },
-      {
-        path: 'profile',
-        component: ProfileComponent,
-        data: { title: 'User profile' },
-      },
-      {
-        path: 'users',
-        component: UsersComponent,
-        data: { title: 'Users' },
-        canActivate: [AdminGuard],
-      },
-      {
-        path: 'hospitals',
-        component: HospitalsComponent,
-        data: { title: 'Hospitals' },
-      },
-      {
-        path: 'doctors',
-        component: DoctorsComponent,
-        data: { title: 'Doctors' },
-      },
-      {
-        path: 'doctor/:id',
-        component: DoctorComponent,
-        data: { title: 'Doctor' },
-      },
-    ],
+    canLoad: [AuthGuard],
+    loadChildren: () =>
+      import('./child-routes.module').then((m) => m.ChildRoutesModule),
   },
 ];
 
